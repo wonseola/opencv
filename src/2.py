5def661b6f5797d0b7f8b8652292df0c473bec5f
WonSeola <39373661+wonseola@users.noreply.github.com>
2023-06-08 22:11:43 +0900

    .

--- src/2.py
+++ src/2.py
@@ -10,9 +10,9 @@ h = int(rows/5)
 for j in range(7):
     cv2.line(img, (j*w,0),(j*w,rows), (255,255,255))
     for i in range(5):
-        cv2.line(img, (cols,h*i),(0,h*i), (255,255,255))
-        # cv2.line(img, [j*w, i*h], [j*w, i*h], [255, 255, 255]) #아 모르겠,,,
+        cv2.line(img, (cols,h*i),(0,h*i), (255,255,255)) 
         cv2.putText(img,(f'{i},{j}'), [j * w, i * h], cv2.QT_FONT_NORMAL, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        #???????????????????아
 
 cv2.imshow('d', img)
 cv2.waitKey(0)
