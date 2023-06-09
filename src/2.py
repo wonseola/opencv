239dec45a247db7c82472e7d961f5709a5c0fedb
WonSeola <39373661+wonseola@users.noreply.github.com>
2023-06-09 12:02:48 +0900

    coooooool

--- src/2.py
+++ src/2.py
@@ -6,21 +6,21 @@ def line(img, a, b):
     # print(f"{rows}  , {cols}")
     w, h = int(cols/a) ,int(rows/b)
     # print(f"{w}  , {h}")
-    for j in range(7):
+    for j in range(a):
         cv2.line(img, (j*w,0),(j*w,rows), (255,255,255))
-        for i in range(5):
+        for i in range(b):
             cv2.line(img, (cols,h*i),(0,h*i), (255,255,255)) 
-            cv2.putText(img,(f'{i},{j}'), [j * w, i * h + 15], cv2.QT_FONT_NORMAL, 0.5, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(img,(f"{i} {j}"), [j * w, i * h + 15], cv2.QT_FONT_NORMAL, 0.5, (255,255,255), 1, cv2.LINE_AA)
             x, y= j * w, i * h
             roi = img[y:y+h, x:x+w] 
             m = cv2.mean(roi)
-            img_mean = (m[0]+ m[1]+ m[2])
-            aa = int(img_mean)
+            img_mean = int(m[0]+ m[1]+ m[2])
             # print(f"{img_mean}")
-            cv2.putText(img,(f'{aa}'), [j * w + 20, i * h + 50], cv2.QT_FONT_NORMAL, 0.5, (100,100,0), 1, cv2.LINE_AA)
+            cv2.putText(img,(f"{img_mean}"), [j * w + 20, i * h + 50], cv2.QT_FONT_NORMAL, 0.5, (128,255,0), 1, cv2.LINE_AA)
 
-img = cv2.imread('./res/1.jpg')
+img = cv2.imread('./res/222.jpg')
 line(img,7,5)
+
 
 cv2.imshow('d', img)
 cv2.waitKey(0)
